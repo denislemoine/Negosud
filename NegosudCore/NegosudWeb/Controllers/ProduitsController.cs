2e9aba83d06db5a0af3a1d9406d09362815abe45
Paulo <paul.lefebvre@viacesi.fr>
2021-03-09 15:29:44 +0100

    update product web

---
+++ NegosudCore/NegosudWeb/Controllers/ProduitsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NegosudWeb.Controllers
+{
+    public class ProduitsController : Controller
+    {
+        // GET: Produits
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- NegosudCore/NegosudWeb/Models/ErrorViewModel.fs
+++
@@ -1,9 +0,0 @@
-namespace NegosudWeb.Models
-
-open System
-
-type ErrorViewModel =
-    { RequestId: string }
-
-    member this.ShowRequestId =
-        not (String.IsNullOrEmpty(this.RequestId))
