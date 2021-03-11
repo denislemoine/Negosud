eb72fc8cf51456bff70272201d36224d2394c9fb
Paulo <paul.lefebvre@viacesi.fr>
2021-03-11 23:38:35 +0100

    add to cart product web

---
+++ NegosudCore/NegosudWeb/Assets/js/cart.js
@@ -0,0 +1,7 @@
+﻿let cartJson = JSON.parse(localStorage.getItem("cart"));
+
+if (cartJson) {
+    document.getElementById("cartCounter").innerHTML = cartJson.length;
+} else {
+    document.getElementById("cartCounter").innerHTML = 0;
+}
--- NegosudCore/NegosudWeb/Assets/js/catalogue.js
+++ NegosudCore/NegosudWeb/Assets/js/catalogue.js
@@ -81,52 +81,7 @@
 		makeCarousel(el);
 	});
 
-	$('.add-cart-large').each(function (i, el) {
-		$(el).click(function () {
-			var carousel = $(this).parent().parent().find(".carousel-container");
-			var img = carousel.find('img').eq(carousel.attr("rel"))[0];
-			var position = $(img).offset();
-
-			var productName = $(this).parent().find('h4').get(0).innerHTML;
-
-			$("body").append('<div class="floating-cart"></div>');
-			var cart = $('div.floating-cart');
-			$("<img src='" + img.src + "' class='floating-image-large' />").appendTo(cart);
-
-			$(cart).css({ 'top': position.top + 'px', "left": position.left + 'px' }).fadeIn("slow").addClass('moveToCart');
-			setTimeout(function () { $("body").addClass("MakeFloatingCart"); }, 800);
-
-			setTimeout(function () {
-				$('div.floating-cart').remove();
-				$("body").removeClass("MakeFloatingCart");
-
-
-				var cartItem = "<div class='cart-item'><div class='img-wrap'><img src='" + img.src + "' alt='' /></div><span>" + productName + "</span><strong>$39</strong><div class='cart-item-border'></div><div class='delete-item'></div></div>";
-
-				$("#cart .empty").hide();
-				$("#cart").append(cartItem);
-				$("#checkout").fadeIn(500);
-
-				$("#cart .cart-item").last()
-					.addClass("flash")
-					.find(".delete-item").click(function () {
-						$(this).parent().fadeOut(300, function () {
-							$(this).remove();
-							if ($("#cart .cart-item").size() == 0) {
-								$("#cart .empty").fadeIn(500);
-								$("#checkout").fadeOut(500);
-							}
-						})
-					});
-				setTimeout(function () {
-					$("#cart .cart-item").last().removeClass("flash");
-				}, 10);
-
-			}, 1000);
-
-
-		});
-	})
+	
 
 	/* ----  Image Gallery Carousel   ---- */
 	function makeCarousel(el) {
@@ -198,43 +153,5 @@
 		});
 	});
 
-	$('.add_to_cart').click(function () {
-		var productCard = $(this).parent();
-		var position = productCard.offset();
-		var productImage = $(productCard).find('img').get(0).src;
-		var productName = $(productCard).find('.product_name').get(0).innerHTML;
 
-		$("body").append('<div class="floating-cart"></div>');
-		var cart = $('div.floating-cart');
-		productCard.clone().appendTo(cart);
-		$(cart).css({ 'top': position.top + 'px', "left": position.left + 'px' }).fadeIn("slow").addClass('moveToCart');
-		setTimeout(function () { $("body").addClass("MakeFloatingCart"); }, 800);
-		setTimeout(function () {
-			$('div.floating-cart').remove();
-			$("body").removeClass("MakeFloatingCart");
-
-
-			var cartItem = "<div class='cart-item'><div class='img-wrap'><img src='" + productImage + "' alt='' /></div><span>" + productName + "</span><strong>$39</strong><div class='cart-item-border'></div><div class='delete-item'></div></div>";
-
-			$("#cart .empty").hide();
-			$("#cart").append(cartItem);
-			$("#checkout").fadeIn(500);
-
-			$("#cart .cart-item").last()
-				.addClass("flash")
-				.find(".delete-item").click(function () {
-					$(this).parent().fadeOut(300, function () {
-						$(this).remove();
-						if ($("#cart .cart-item").size() == 0) {
-							$("#cart .empty").fadeIn(500);
-							$("#checkout").fadeOut(500);
-						}
-					})
-				});
-			setTimeout(function () {
-				$("#cart .cart-item").last().removeClass("flash");
-			}, 10);
-
-		}, 1000);
-	});
 });
